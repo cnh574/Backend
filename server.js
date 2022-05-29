2b925f217a107a0f6c9db8ed420e294d60678c4a
John Komninos <johnkomninos@johns-air.myfiosgateway.com>
2022-05-29 12:36:39 -0400

    saving before a pull

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(express.json()); // returns middleware that only parses JSON - may or ma
 //___________________
 //localhost:3000
 app.get("/", (req, res) => {
-  res.send("Not Hello World!");
+  res.json("Not Hello World!");
 });
 
 //___________________
