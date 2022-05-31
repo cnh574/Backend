bede126bfc619ecaa4f7a4232dbe2438fbba47d8
CANDACE HARRIS <candaceharris@CANDACEs-MBP.lan>
2022-05-31 09:51:44 -0400

    backend routes created

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ db.on("disconnected", () => console.log("mongo disconnected"));
 //___________________
 
 //use public folder for static assets
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+// app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 app.use(cors());
 //use method override
@@ -60,6 +60,25 @@ app.post("/", (req, res) => {
   Beach.create(req.body, (err, createdBeach) => {
     res.json(createdBeach);
   });
+});
+
+// Delete Route
+app.delete("/:id", (req, res) => {
+  Beach.findByIdAndRemove(req.params.id, (err, deletedBeach) => {
+    res.json(deletedBeach);
+  });
+});
+
+// Edit Route
+app.put("/:id", (req, res) => {
+  Beach.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedBeach) => {
+      res.json(updatedBeach);
+    }
+  );
 });
 
 //___________________
